8b9dc3e34774f3d2084fb878c77371ba4d185b9b
Seoyun <sylph0105@naver.com>
2022-02-22 16:57:12 +0900

    [Update] HMWK 03. 캐러셀(이미지 슬라이드) 만들기_2

--- hmwk_03/classList.js
+++ hmwk_03/classList.js
@@ -108,17 +108,38 @@ setInterval(function(){
 }, 1000);
 
 // 중앙 : 이미지 슬라이드
+var slide = 1;
 $('.slide-1').on('click', function(){
     $('.slide-container').css('transform', 'translateX(0vw)');
-    $('.slide-container').css('transition', 'all 1s');
+    slide = 1;
 })
 
 $('.slide-2').on('click', function(){
     $('.slide-container').css('transform', 'translateX(-100vw)');
-    $('.slide-container').css('transition', 'all 1s');
+    slide = 2;
 })
 
 $('.slide-3').on('click', function(){
     $('.slide-container').css('transform', 'translateX(-200vw)');
-    $('.slide-container').css('transition', 'all 1s');
+    slide = 3;
+})
+
+$('.prev').on('click', function(){
+    if (slide == 3){
+        $('.slide-container').css('transform', 'translateX(-100vw)');
+        slide = 2;
+    } else if (slide == 2){
+        $('.slide-container').css('transform', 'translateX(-0vw)');
+        slide = 1;
+    }
+})
+
+$('.next').on('click', function(){
+    if (slide == 1){
+        $('.slide-container').css('transform', 'translateX(-100vw)');
+        slide = 2;
+    } else if (slide == 2){
+        $('.slide-container').css('transform', 'translateX(-200vw)');
+        slide = 3;
+    }
 })
